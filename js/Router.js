86df90672d1c8a125c7d17e5cc67c6fc4bd2e84c
Josh Brzinski <jbbrzinski@gmail.com>
2015-11-04 15:37:27 -0500

    Added Router.js

---
+++ js/Router.js
@@ -0,0 +1,13 @@
+
+let Router = Backbone.Router.extend({
+  routes: {
+    ""              : "home",
+    'register'      : "registerView", 
+    "deck"          : "userView",
+    "deck/:deckID"  : "deckView",
+    "addDeck"       : "addDeck"
+    "card/:cardID"  : "imageView",
+    "addCard"       : "addCard"
+  },
+
+});
