99a074b161ac257cb565d9c8117eeaa270d0dc19
Josh Brzinski <jbbrzinski@gmail.com>
2015-11-05 14:03:44 -0500

    Fixed missing import/export.  Added index.html app div.  Modified register and userViews.

--- js/Router.js
+++ js/Router.js
@@ -1,23 +1,27 @@
+import $ from 'jquery';
+import _ from 'underscore';
+import moment from 'moment';
+import Backbone from 'backbone';
+import React from 'react';
+import ReactDom from 'react-dom';
+import {HomeView,RegView,UserView} from './views'
+
 
 let Router = Backbone.Router.extend({
   routes: {
     ''              : 'home',
-    'register'      : 'register'
-    'deck'          : 'userView',
-    'deck/:deckID'  : 'deckView',
-    'addDeck'       : 'addDeck',
-    'card/:cardID'  : 'imageView',
-    'addCard'       : 'addCard'
+    'register'      : 'register',
+    // 'deck'          : 'userView',
+    // 'deck/:deckID'  : 'deckView',
+    // 'addDeck'       : 'addDeck',
+    // 'card/:cardID'  : 'imageView',
+    // 'addCard'       : 'addCard'
   },
 
-  start() {
-    Backbone.history.start();
-  },
-
-    initialize(appElement) {
+  initialize(appElement) {
     this.el = appElement;
-    this.deck = new deckCollection();
-    this.card = new cardCollection();
+    // this.deck = new deckCollection();
+    // this.card = new cardCollection();
     this.user = new userCollection();
     let router = this;
   },
@@ -30,23 +34,38 @@ let Router = Backbone.Router.extend({
     ReactDom.render(component, this.el);
   },
 
-  logout() {
-    
-  }
 
-  register() {
-    //FIXME Function below belongs on actual Register VIEW. 
-    // let request = $.ajax({
-    //   url: 'http://localhost:3000/login',
-    //   method: 'POST',
-    //   data: {
-    //     user: {
-    //       username: '',
-    //       full_name: '',
-    //       password: '',
-    //     }
-    //   }
-  }
+  loginRequest(){
+    let request = $.ajax({
+      url: 'https://rocky-garden-9800.herokuapp.com',
+      method: 'POST',
+      data: {
+        user: {
+          username: $('.username').val(),
+          password: $('.password').val()
+        }
+      }
+    });
+  },
+
+  // logout() {
+    
+  // },
+
+  registerRequest() {
+    let request = $.ajax({
+      url: 'peaceful-water-4820.herokuapp.com',
+      method: 'POST',
+      data: {
+        user: {
+          username: $('.username').val(),
+          password: $('.password').val(),
+          full_name: $('.fullName').val(),
+          email: $('.email').val()
+        }
+      }
+    });
+  },
 
   home() {
     this.user.fetch().then(() => {
@@ -54,9 +73,15 @@ let Router = Backbone.Router.extend({
         onHomeClick={() => this.goto('')}
         onLoginClick={() => this.goto('login')}
         onLogoutClick={()=> this.goto('logout')}
-        onRegisterClick={() => this.goto('register')}
-      );
+        onRegisterClick={() => this.goto('register')}/>
+        )
     });
   },
 
+  start() {
+    Backbone.history.start();
+  },
+
 });
+
+export default Router;
---
+++ js/views/register.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Backbone from 'backbone';
+import $ from 'jquery';
+
+
+export default React.createClass({
+
+  render() {},   
+});
