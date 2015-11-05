aaf14e67fafba142cbc0e9493319fedf3db07263
John Rae <john.rae23@gmail.com>
2015-11-05 14:08:25 -0500

    edit navbar

---
+++ js/views/nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default React.createClass({
+
+  render() {
+    return (
+      <div className="navbar">
+        <button onNavigate={}>Home</button>
+        <button>Logout</button>
+      </div>
+    );
+  }
+}); 
